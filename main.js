9b4ee3335e9448c23038611620f364d5f2fdb4a0
Eduardo Mejia Pleitez <eduardo.77.1996@gmail.com>
2022-12-05 18:56:18 -0600

    :sparkles: Update main.js

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ submitbtn.addEventListener("click", function(event){ event.preventDefault(); });
 /* Add Map with MapBox */
 placeLocation()
 function placeLocation(){
-  mapboxgl.accessToken = 'pk.eyJ1Ijoid2FyaW9tZWppYSIsImEiOiJja24zbTE2Z3kxaXp3Mm90cHFtbnRxNWUyIn0.oVOxccs1_pWuZLIYsVxgrA';
+  mapboxgl.accessToken = 'pk.eyJ1Ijoid2FyaW9tZWppYSIsImEiOiJjbGJiZ3ZqYnoxZG5xM29vMnc1c3o2NGFvIn0.GaW8s2vErdDDvkpunYySIQ';
   const myMap = new mapboxgl.Map({
     container: 'myMap',
     style: 'mapbox://styles/mapbox/streets-v11',
